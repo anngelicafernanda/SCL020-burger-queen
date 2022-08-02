681399c4d7fae8aa975e7e3744100ab368ec592b
Angelica Contreras <afernandac94@gmail.com>
2022-08-02 18:02:44 -0400

    Vista Chef

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
-import './Footer.css'
+import "./Footer.css";
+import NormalButton from "../NormalButton/NormalButton";
+import { Link } from "react-router-dom";
+import NextStepImage from "../../imagesApp/mover-al-siguiente.png";
+
 const Footer = () => {
   return (
-    <footer className='footer'>
-        <p>“Una experiencia en sabores a la que querrás volver”</p>
+    <footer className="footer">
+      <p>“Una experiencia en sabores a la que querrás volver”</p>
+      <Link to="/chef">
+        <NormalButton icon={NextStepImage} text="Vista Chef" />
+      </Link>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
