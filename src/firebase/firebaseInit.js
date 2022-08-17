89e655fadf77faadfc6dfae47173243690a8de81
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-08-16 21:11:46 -0400

    creación de ordenes en base de datos

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,17 +1,26 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+// Importamos la aplicacion
+import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebaseConfig.js";
+
+
+// Importamos el modulo de autenticacion
 import {
   getAuth,
   signInWithEmailAndPassword,
   signOut,
-} from "https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js";
+} from "firebase/auth";
 
+// Importamos el modulo de firestore
+import { getFirestore } from 'firebase/firestore'
+
+// Inicializamos todos los modulos
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-// const db = getFirestore(app);
+const db = getFirestore(app);
 
 export {
   auth,
   signInWithEmailAndPassword,
   signOut,
+  db
 };
